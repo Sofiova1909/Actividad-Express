c0b4ce0dcb1c64d279b197693c089dd278431cd5
Maria Sofia Ramirez Ovalle <mariskate1997@gmail.com>
2026-07-24 11:48:10 -0500

    Configuracion variable de entorno, parte 2

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 import express from "express"
 const app = express();
-const port = 3000;
+const port = process.env.PUERTO || 3000
 app.get("/", function(req, res) {
-res.send("Aprendiendo express,ficha 84081");
+res.send("Hola aprendiendo express, con la ficha 3407181, ADSO en el SENA");
 });
 app.listen(port, function() {
     console.log(`Servidor en funcionamiento en el puerto ${port}`);
