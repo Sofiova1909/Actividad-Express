b1c689bd94fe08e5ac262d304dc689545cd4377b
Maria Sofia Ramirez Ovalle <mariskate1997@gmail.com>
2026-07-31 10:49:11 -0500

    Creacion nuevo endpoint otraruta send - json

--- app.js
+++ app.js
@@ -6,9 +6,24 @@ configDotenv()
 const app = express();
 const puerto = process.env.PUERTO || 3030
 
+
 app.get("/", function(req, res) {
 res.send("Hola aprendiendo express, con la ficha 3407181, ADSO en el SENA hoy  31 de julio")
 });
+
+//otro endpoint
+app.get("/otraruta", (req, res)=>{
+    res.send(`<h1>Otro ejemplo de ruta</h1>
+        <h2>End point con res.semd</h2>
+        `)
+})
+
+//otro endpoint
+app.get("/ruta2", (req, res)=>{
+    res.json({"nombre": "sofia", "Apellido": "Guerrero", "Cargo": "Instructor"})
+})
+
 app.listen(puerto, function() {
-    console.log(`Servidor en funcionamiento en el puerto ${puerto}`)
+    console.log(`SERVIDOR: http://localhost:${puerto}`)
 });
+
